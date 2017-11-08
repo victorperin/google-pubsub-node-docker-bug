44ca7bda4f2d04810b8bc363041f20b4cfd3ef77
Victor Perin <me@victorperin.ninja>
2017-11-08 14:22:41 -0200

    feat(tests): adding tests to @google-cloud/common-grpc dependencies

--- test.js
+++ test.js
@@ -72,6 +72,69 @@ it('should continue executing after uuid call', () => {
   executed.should.be.true;
 });
 
+it('should continue executing after @google-cloud/common call', () => {
+  let executed = false;
+  const PubSub = require('@google-cloud/common');
+  executed = true;
+  executed.should.be.true;
+});
+
+it('should continue executing after dot-prop call', () => {
+  let executed = false;
+  const PubSub = require('dot-prop');
+  executed = true;
+  executed.should.be.true;
+});
+
+it('should continue executing after duplexify call', () => {
+  let executed = false;
+  const PubSub = require('duplexify');
+  executed = true;
+  executed.should.be.true;
+});
+
+it('should continue executing after extend call', () => {
+  let executed = false;
+  const PubSub = require('extend');
+  executed = true;
+  executed.should.be.true;
+});
+
+it('should continue executing after is call', () => {
+  let executed = false;
+  const PubSub = require('is');
+  executed = true;
+  executed.should.be.true;
+});
+
+it('should continue executing after modelo call', () => {
+  let executed = false;
+  const PubSub = require('modelo');
+  executed = true;
+  executed.should.be.true;
+});
+
+it('should continue executing after retry-request call', () => {
+  let executed = false;
+  const PubSub = require('retry-request');
+  executed = true;
+  executed.should.be.true;
+});
+
+it('should continue executing after through2 call', () => {
+  let executed = false;
+  const PubSub = require('through2');
+  executed = true;
+  executed.should.be.true;
+});
+
+it('should continue executing after grpc call', () => {
+  let executed = false;
+  const PubSub = require('grpc');
+  executed = true;
+  executed.should.be.true;
+});
+
 it('should continue executing after @google-cloud/common-grpc call', () => {
   let executed = false;
   const PubSub = require('@google-cloud/common-grpc');
